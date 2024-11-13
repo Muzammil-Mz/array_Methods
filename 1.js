35c3600897b70623d8e7f56add66989a813cbd8a
Muzammil-Mz <Mujjuimran74@gmail.com>
2024-11-13 17:13:01 +0000

    13

---
+++ 1.js
@@ -0,0 +1,11 @@
+//at this access the elemtnts using the index number and element is seen as output
+// let students=["junaid",'kishore',"abdul","suhail"]
+// console.log(students.at(3));
+
+
+// let fruits=["apple","ball","cat"]
+// console.log(fruits.at(2));
+
+// let books = [["maths", ["bio", "physics"]], ["chem"]];
+// console.log(books[1][0]);  // Accessing "chem"
+// console.log(books[0][1][1]);  // Accessing "physics"
