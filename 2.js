0b71cc7d9b9c478b11a88f1939bd76178756453f
Muzammil-Mz <Mujjuimran74@gmail.com>
2024-11-13 17:18:40 +0000

    13

---
+++ 2.js
@@ -0,0 +1,9 @@
+// concat - joins 2 arrays
+// let studetns=["junaid","kishoe","saleem","numan"]
+// let teachers=["pt","sport","bio"]
+// let staff=teachers.concat(studetns)
+// console.log(staff);
+
+let sports=["basket","foot","volley"]
+let indoor=["caroom","ping"]
+console.log(sports.concat(indoor));
